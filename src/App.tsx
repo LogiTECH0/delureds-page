e045485330363d01ac1dcb456ec017bff0663cd8
Delured <nahnybidailia@gmail.com>
2026-04-05 19:40:15 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,13 @@ function App() {
   const soloprojects: ProjectType[] = [
     { name: "Chromify", link: "https://logitech0.github.io/chromify/", description: "Cross-platform web-based graphics editor", img: "/chromify.png" },
     { name: "Professional Calculator", link: "/calculator.py", description: "Calculator but better, you can calculate complex expressions", img: "/professionalcalc.jpg" },
-    { name: "Weatherly", link: "https://weatherly-comfort.netlify.app/", description: "Check the weather for your location easily", img: "/weatherly.jpg" },
-    { name: "GitViewer", link: "https://git-views.netlify.app/", description: "View any person's github profile including their bio, repositories, followers", img: "/gitviewer.jpg" },
-    { name: "MemoCards", link: "https://game-memo-cards.netlify.app/", description: "Memory cards game and personally my favorite project so far", img: "/memocards.jpg" },
+    { name: "Weatherly", link: "https://weatherly-nine-delta.vercel.app/", description: "Check the weather for your location easily", img: "/weatherly.jpg" },
+    { name: "GitViewer", link: "https://gitviewer-beryl.vercel.app/", description: "View any person's github profile including their bio, repositories, followers", img: "/gitviewer.jpg" },
+    { name: "MemoCards", link: "https://game-memo-cards.vercel.app/", description: "Memory cards game and personally my favorite project so far", img: "/memocards.jpg" },
+    { name: "Expense Tracker", link: "https://expense-tracker-d-theta.vercel.app/", description: "Track your income and expenses with simple analytics and history", img: "/expensetracker.jpg" },
+    { name: "Flip & Learn", link: "https://flip-n-learn.vercel.app/", description: "Flashcards app for learning and memorizing information effectively", img: "/flipnlearn.jpg" },
+    { name: "CineScope", link: "https://cine-scope-iota-ten.vercel.app/", description: "Explore popular, highly rated movies and TV shows with details and posters", img: "/cinescope.jpg" },
+    { name: "Link Shortener", link: "https://shortener-d.vercel.app/", description: "Simple URL shortener with history and React Router-based navigation", img: "/linkshortener.jpg" },
   ]
   const teamprojects: ProjectType[] = [
     { name: "Leleka", link: "https://deliveryleleka.netlify.app/", description: "Personal ukrainian messager with bigger privacy options", img: "/leleka.jpg", role: "Frontend Developer" },
